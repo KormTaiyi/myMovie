7e25bdadef3542f25aa4f5a65ffa99c7fdf6e99b
K-taiyi <korm.taiyi289@gmail.com>
2025-02-07 13:40:57 +0700

    a

--- src/components/HeadCom.jsx
+++ src/components/HeadCom.jsx
@@ -6,10 +6,10 @@ const HeadCom = () => {
   return (
     <header className="w-full">
         <div className="w-full bg-(--primary-color)">
-            <div  className="w-[90%] m-auto flex items-center justify-between">
+            <div  className="w-full px-1 sm:w-[90%] m-auto flex items-center justify-between">
                 <ul type="none" className="flex items-center gap-2">
                     <div className="flex items-center">
-                        <FaBars className="text-lg text-white font-bold"/>
+                        {/* <FaBars className="text-lg text-white font-bold"/> */}
                         <Link to="/"><img src={logo} alt="" className="w-22"/></Link>
                     </div>
                     <ul type="none" className="flex gap-4">
--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
       className="h-[455px] bg-center bg-no-repeat bg-cover relative"
       style={{ backgroundImage: `url(${backgroundImage})` }}>
         <div className="w-full h-full bg-black opacity-50"></div>
-        <div className="absolute top-[35%] left-[10px] sm:left-[80px] md:left-[50px]">
+        <div className="absolute top-[35%] left-[10px] sm:left-[60px]">
             <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold bg-clip-text text-transparent bg-linear-to-br from-teal-300 to-blue-600">That's a <br /> Wrap 2024</h1>
             <p className="text-white mt-4 text-sm sm:text-xl">The best (and worst) of the year from TMDB.</p>
         </div>
