e9d0d0a25fd9b49910fb4dd83bb3ba563074b222
K-taiyi <korm.taiyi289@gmail.com>
2025-02-08 14:16:30 +0700

    a

--- src/components/home/TrailerPage.jsx
+++ src/components/home/TrailerPage.jsx
@@ -1,6 +1,48 @@
+import { Link } from "react-router"
+
 const TrailerPage = () => {
   return (
-    <div>TrailerPage</div>
+    // hover change background img
+    <section className="w-full bg-green-200 pt-5">
+      <div className="w-full sm:w-[90%] m-auto sm:px-0 px-2">
+        <div className="flex gap-2 sm:gap-4 md:gap-6 mb-6">
+          <h1 className="text-lg md:text-2xl">Latest Trailers</h1>
+          {
+            <div className="flex items-center sm:gap-4 gap-3 border-1 rounded-2xl">
+              <p className="cursor-pointer pl-2 md:text-lg text-sm bg-clip-text text-transparent bg-linear-to-br from-teal-300 to-purple-600">Popular</p>
+              <p className="cursor-pointer pr-2 md:text-lg text-sm bg-clip-text text-transparent bg-linear-to-br from-teal-300 to-blue-600">In Theaters</p>
+            </div>
+          }
+        </div>
+        <div className="w-full">
+          <div className="overflow-hidden">
+              {
+                // data.map(d=>(
+                  <div className="flex gap-10 overflow-x-auto">
+                    <div className="w-[250px] sm:w-[300px] overflow-hidden rounded-xl shrink-0 mb-5">
+                      {/* <Link to="" className=" overflow-hidden rounded-xl"> */}
+                        <div className="overflow-hidden block rounded-xl hover:scale-105 duration-200">
+                          <iframe 
+                            src="https://www.youtube.com/embed/50NudnPC_Qw?mute=0" 
+                            className="w-full rounded-xl h-[170px]"
+                            allow="encrypted-media" 
+                            allowFullScreen
+                            title="Trailer Video"
+                          />
+                        </div>
+                      {/* </Link> */}
+                      <div className="w-full min-h-[60px] p-2 text-center">
+                        <Link to="" className="text-white font-bold text-xl md:text-2xl">Title Goes Here</Link>
+                        <p className="text-white text-2xs md:text-xl">Description Goes Here</p>
+                      </div>
+                    </div>
+                  </div>
+                // ))
+              }
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/home/TrendingPage.jsx
+++ src/components/home/TrendingPage.jsx
@@ -18,28 +18,30 @@ const TrendingPage = () => {
   },[])
   return (
     <main className="w-full py-5">
-      <div className="w-[90%] m-auto">
+      <div className="m-auto w-full sm:w-[90%] sm:px-0 px-2">
         <div className="flex gap-4 mb-6">
-          <h1 className="text-2xl">Trending</h1>
-          <div className="flex items-center gap-4 border-1 rounded-2xl">
-            <p className="cursor-pointer pl-2 text-md bg-clip-text text-transparent bg-linear-to-br from-teal-300 to-purple-600">Today</p>
-            <p className="cursor-pointer pr-2 text-md bg-clip-text text-transparent bg-linear-to-br from-teal-300 to-blue-600">This Week</p>
-          </div>
+          <h1 className="text-lg md:text-2xl">Trending{}</h1>
+            {
+              <div className="flex items-center gap-4 border-1 rounded-2xl">
+                <p className="cursor-pointer pl-2 md:text-lg text-sm bg-clip-text text-transparent bg-linear-to-br from-teal-300 to-purple-600">Today</p>
+                <p className="cursor-pointer pr-2 md:text-lg text-sm bg-clip-text text-transparent bg-linear-to-br from-teal-300 to-blue-600">This Week</p>
+              </div>
+            }
         </div>
         <div className="w-full">
           <div className="overflow-hidden">
               {
-                data.map(d=>(
+                // data.map(d=>(
                   <div className="flex gap-5 overflow-x-auto">
-                    <div className="w-[160px] shrink-0">
-                        {/* <Link to=""><img src={} alt="" className="w-full rounded-xl h-[220px] bg-red-300"/></Link> */}
+                    <div className="w-[130px] sm:w-[160px] shrink-0">
+                        <Link to=""><img src="" alt="" className="w-full rounded-xl h-[190px] sm:h-[220px] bg-red-300"/></Link>
                         <div className="w-full min-h-[60px] p-3">
-                          <Link to="" className="font-bold text-md">{}</Link>
-                          <p className="text-gray-800 font-thin text-md">{}</p>
+                          <Link to="" className="font-bold text-md">title{}</Link>
+                          <p className="text-gray-800 font-thin text-md">date{}</p>
                         </div>
                     </div>
                   </div>
-                ))
+                // ))
               }
           </div>
         </div>
