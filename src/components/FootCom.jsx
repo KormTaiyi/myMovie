1281ff390ef4a1c24c6b462f2b726927b48bdeee
K-taiyi <korm.taiyi289@gmail.com>
2025-02-08 12:51:52 +0700

    a

--- src/components/FootCom.jsx
+++ src/components/FootCom.jsx
@@ -1,6 +1,38 @@
+import { Link } from "react-router"
+import logo from "../assets/logo.png"
 const FootCom = () => {
   return (
-    <div>FootCom</div>
+    <footer className="bg-(--primary-color) py-8">
+      <div className="md:w-full w-[90%] h-auto m-auto md:flex items-start md:justify-between px-6">
+        <img src={logo} alt="" className="w-20 md:mb-0 mb-4 object-contain md:m-0 m-auto"/>
+        <div className="flex text-white gap-10 items-center justify-between flex-col sm:flex-row sm:text-left text-center">
+          <ul type="none" className="">
+            <h1 className="text-lg md:text-xl mb-4">The Basics</h1>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">About TMDB</Link></li>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">Contact Us</Link></li>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">Support Forums</Link></li>
+          </ul>
+          <ul type="none" className="">
+            <h1 className="text-lg md:text-xl mb-4">Get Involved</h1>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">Contribution Bible</Link></li>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">Add New Movie</Link></li>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">Add New TV Show</Link></li>
+          </ul>
+          <ul type="none" className="">
+            <h1 className="text-lg md:text-xl mb-4">Community</h1>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">Guidelines</Link></li>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">Discussions</Link></li>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">Leaderboard</Link></li>
+          </ul>
+          <ul type="none" className="">
+            <h1 className="text-lg md:text-xl mb-4">Legal</h1>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">Terms of Use</Link></li>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">Privacy Policy</Link></li>
+            <li><Link to="" className="text-sm md:text-md hover:text-gray-400 hover:underline">DMCA Policy</Link></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/components/home/TrendingPage.jsx
+++ src/components/home/TrendingPage.jsx
@@ -1,6 +1,50 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router"
+
 const TrendingPage = () => {
+  const url = "";
+  const [data,setData] = useState([])
+  useEffect(()=>{
+    async function fetchingData(){
+      try {
+        const response = await fetch(url)
+        const result = await response.json()
+        setData(result)
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchingData();
+  },[])
   return (
-    <div>TrendingPage</div>
+    <main className="w-full py-5">
+      <div className="w-[90%] m-auto">
+        <div className="flex gap-4 mb-6">
+          <h1 className="text-2xl">Trending</h1>
+          <div className="flex items-center gap-4 border-1 rounded-2xl">
+            <p className="cursor-pointer pl-2 text-md bg-clip-text text-transparent bg-linear-to-br from-teal-300 to-purple-600">Today</p>
+            <p className="cursor-pointer pr-2 text-md bg-clip-text text-transparent bg-linear-to-br from-teal-300 to-blue-600">This Week</p>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="overflow-hidden">
+              {
+                data.map(d=>(
+                  <div className="flex gap-5 overflow-x-auto">
+                    <div className="w-[160px] shrink-0">
+                        {/* <Link to=""><img src={} alt="" className="w-full rounded-xl h-[220px] bg-red-300"/></Link> */}
+                        <div className="w-full min-h-[60px] p-3">
+                          <Link to="" className="font-bold text-md">{}</Link>
+                          <p className="text-gray-800 font-thin text-md">{}</p>
+                        </div>
+                    </div>
+                  </div>
+                ))
+              }
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
